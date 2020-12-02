c00373d0f035833dc5b440814931ef3d28708c63
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-02 08:32:13 -0500

    simple set up

---
+++ script.js
@@ -0,0 +1,43 @@
+const quizData = [
+    {
+        question: 'What is the rarest M&M color?',
+        a: 'Brown',
+        b: 'Yellow',
+        c: 'Red',
+        d: 'Green',
+        correct: 'a',
+
+    }, {
+        question: 'In a website browser address bar, what does “www” stand for?',
+        a: 'Woder Wise Woman',
+        b: 'Wiskey World West',
+        c: 'World Wide Web',
+        d: 'Welcome Windy Window',
+        correct: 'c',
+
+    }, {
+        question: 'Which programming language is popular?',
+        a: 'Java',
+        b: 'Python',
+        c: 'C',
+        d: 'Javascript',
+        correct: 'd',
+
+    },  {
+        question: 'What does HTML stands for?',
+        a: 'Hypertext Markup language',
+        b: 'Cascading Style Sheets',
+        c: 'Human Time Machine Language',
+        d: 'Jason object Notation',
+        correct: 'a',
+
+    },  {
+        question: 'What year JS was launched?',
+        a: '2000',
+        b: '1990',
+        c: '1995',
+        d: '2010',
+        correct: 'c',
+        
+    },
+]
