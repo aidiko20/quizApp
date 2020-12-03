9dad84a92f50261744ced5d44f8c581d7c870779
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-03 14:28:46 -0500

    finsihed all the logic

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const quizData = [
         d: 'Javascript',
         correct: 'd',
 
-    },  {
+    }, {
         question: 'What does HTML stands for?',
         a: 'Hypertext Markup language',
         b: 'Cascading Style Sheets',
@@ -31,13 +31,75 @@ const quizData = [
         d: 'Jason object Notation',
         correct: 'a',
 
-    },  {
+    }, {
         question: 'What year JS was launched?',
         a: '2000',
         b: '1990',
         c: '1995',
         d: '2010',
         correct: 'c',
-        
+
     },
-]
+];
+
+let currentQuiz = 0;
+let score = 0;
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById('question');
+const a_answer = document.getElementById('a_answer');
+const b_answer = document.getElementById('b_answer');
+const c_answer = document.getElementById('c_answer');
+const d_answer = document.getElementById('d_answer');
+const submitBtn = document.getElementById('submit');
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_answer.innerText = currentQuizData.a;
+    b_answer.innerText = currentQuizData.b;
+    c_answer.innerText = currentQuizData.c;
+    d_answer.innerText = currentQuizData.d;
+
+};
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.<h2>
+
+            <button onclick="location.reload()">Reload</button>`;
+        }
+    }
+});
